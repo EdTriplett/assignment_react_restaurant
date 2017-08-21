9712a433ea75fd34b979e0899d8fbb5234da5f3c
Greg Filipczak <gregfilipczak@gmail.com>
2017-08-21 17:18:05 +0000

    navbar

---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-light">
+          <h1>Header</h1>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Nav;
---
+++ src/Test.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+class Test extends React.Component{
+  render() {
+    return (
+      <div>
+        <p>This is a test</p>
+        
+      </div>
+    );
+  }
+}
+
+export default Test;
