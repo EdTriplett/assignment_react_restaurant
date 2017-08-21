1ab7bd88a5631224bfb34c93ef109d6ed814232d
EdTriplett <EdTriplett@users.noreply.github.com>
2017-08-21 13:40:56 -0400

    navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,21 @@
 import React from "react";
 
+const Link = ({name})=> {
+  return (
+    <li className="nav-item"><a className="nav-link" href={name}>{name}</a></li>
+    )
+}
+
+
 class Nav extends React.Component {
   render() {
     return (
       <div>
         <nav className="navbar navbar-light">
-          <h1>Header</h1>
+          <ul className="nav navbar-nav">
+            <Link name="menu"/> 
+            <Link name="reservation"/>
+          </ul>
         </nav>
       </div>
     );
